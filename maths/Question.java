262985721b30899c34116b2fc6cb899eefbbd3b0
Avi Greenbury <avi@moses.(none)>
2010-06-01 22:25:24 +0100

    Made into a NetBeans project, added Arithmetic class. Need to work out how to pick classes(subjects) at 'random'.

--- maths/Question.java
+++ maths/Question.java
@@ -1,43 +1,18 @@
-import java.util.*;
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
 
-public class Question{
+/**
+ *
+ * @author avi
+ */
+public interface Question {
 
-	Random generator = new Random();
-	int a = generator.nextInt(100);
-	int b = generator.nextInt(100);
-	String question = a+"+"+b;
-	int answer = a + b;
 
-	static int tot = 0;
-	static int totWrong = 0 ;
-	static int totCorrect = 0;
+   //[int|String] answer
+    String correct = "";
+    String ask();
+    Boolean guess(String guess);
 
-	public Question(){
-	}
-
-	public String ask(){
-		tot++;
-		StandardQuestion Q = new Differentiation();
-		return Q.ask();
-	}
-
-	public boolean answer(int guess){
-		if(guess == answer){
-			totCorrect++;
-			return true;
-		}else{
-			totWrong++;
-			return false;
-		}
-	}
-
-        public int correct(){
-		return answer;
-	}
-
-        public static void stats(){
-		System.out.println("Total: "+tot);
-		System.out.println("Correct: "+totCorrect);
-		System.out.println("Wrong: "+totWrong);
-	}
 }
--- maths/maths.java
+++ maths/maths.java
@@ -5,20 +5,23 @@ import java.io.Console;
 public class maths{
 	public static void main(String[] args) {
 
-		for(int count=0; count<10; count++){
+            for(int count=0; count<2; count++){
 
-			Question Q = new Question();		
-			Console c = System.console();
-			String s = c.readLine(Q.ask()+"\n");
-			int guess = Integer.parseInt(s.trim());
-/*			if(Q.answer(guess)){
-				System.out.println("Yay");
-			}else{
-				System.out.println("Nope");
-				System.out.println("\tExpected "+Q.correct());
-			}
-*/		}
-		
-		Question.stats();
+                QuestionMaster Q = new QuestionMaster();
+                Console c = System.console();
+                if (c == null){
+                    System.out.println("Error: no console support!");
+                    System.exit(1);
+                }else{
+                    String guess = c.readLine(Q.ask()+"\n");
+                    if(Q.answer(guess)){
+                        System.out.println("Yay");
+                    }else{
+                        System.out.println("Nope");
+                        System.out.println("\tExpected "+Q.correct);
+                    }
+                }
+            }
+            QuestionMaster.stats();
 	}
 } 
